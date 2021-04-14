3c7c792f46bb9ab5c7400423555e4f3d7146ee94
DetectiveJiang <407148852@qq.com>
2021-04-14 16:26:41 +0800

    upload via server

--- PhaseNet/run_PhaseNet.py
+++ PhaseNet/run_PhaseNet.py
@@ -1,10 +1,21 @@
 import os
+import time
+import run
 
-# os.system("python run.py --mode=pred --model_dir=model/190703-214543 --data_dir=dataset/waveform_pred
-# --data_list=dataset/waveform.csv --output_dir=output --plot_figure --save_result --batch_size=20")
-# os.system("python run.py --mode=pred --model_dir=model/190703-214543 --data_dir=TEST_dataset/waveform_xfj "
-#           "--data_list=TEST_dataset/waveform.csv --output_dir=output --plot_figure --save_result --batch_size=20 "
-#           "--input_length=6000")
-os.system("python run.py --mode=pred --model_dir=model/190703-214543 --data_dir=XFJ_dataset/waveform_xfj "
-          "--data_list=XFJ_dataset/waveform.csv --output_dir=output --plot_figure --save_result --batch_size=500 "
-          "--input_length=6000")
+if __name__ == '__main__':
+    start = time.process_time()
+    args = run.read_args()
+    args.batch_size = 500
+    args.data_dir = '/home/jc/work/XFJ1121/phasenet_input/waveform_xfj'
+    args.data_list = '/home/jc/work/XFJ1121/phasenet_input/waveform.csv'
+    args.output_dir = '/home/jc/work/XFJ1121/phasenet_output'
+    args.input_length = 6000
+    args.mode = 'pred'
+    args.model_dir = 'model/190703-214543'
+    run.main(args)
+    # os.system("python run.py --mode=pred --model_dir=model/190703-214543 "
+    #           "--data_dir=/home/jc/work/XFJ1121/phasenet_input "
+    #           "--data_list=/home/jc/work/XFJ1121/phasenet_input/waveform.csv --output_dir=output --plot_figure "
+    #           "--save_result --batch_size=500 --input_length=6000")
+    end = time.process_time()
+    print(end - start)
